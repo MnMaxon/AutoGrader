91b49f8f04ef0eb0cd2c28579a4f98143d272c76
MnMaxon <masontg777@aol.com>
2020-04-20 23:38:09 -0400

    Removed Example Code from old project

--- src/com/Group4/AutoGrader/Controllers/MainController.java
+++ src/com/Group4/AutoGrader/Controllers/MainController.java
@@ -1,18 +1,21 @@
 package com.Group4.AutoGrader.Controllers;
 
-public class MainController extends Controller {	/**
- * Gives the xml file name, eg: MainView.fxml
- *
- * @return "MainView.fxml"
- */
-@Override
-public String getFileName() {
-	return "MainView.fxml";
-}
-public void manage(){
-	new SelectController().show();
-}
-public void grade(){
-	new RunController().show();
-}
+public class MainController extends Controller {
+	/**
+	 * Gives the xml file name, eg: MainView.fxml
+	 *
+	 * @return "MainView.fxml"
+	 */
+	@Override
+	public String getFileName() {
+		return "MainView.fxml";
+	}
+
+	public void manage() {
+		new SelectController().show();
+	}
+
+	public void grade() {
+		new RunController().show();
+	}
 }
