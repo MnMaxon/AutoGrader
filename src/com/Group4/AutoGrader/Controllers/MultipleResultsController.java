37c0f67684976b9361e98e32517d9d2b22c65d29
MnMaxon <masontg777@aol.com>
2020-04-21 23:57:52 -0400

    Saving to File

--- src/com/Group4/AutoGrader/Controllers/MultipleResultsController.java
+++ src/com/Group4/AutoGrader/Controllers/MultipleResultsController.java
@@ -1,5 +1,6 @@
 package com.Group4.AutoGrader.Controllers;
 
+import com.Group4.AutoGrader.AutoGrader;
 import com.Group4.AutoGrader.Model.Assignment;
 import com.Group4.AutoGrader.Model.Question;
 import javafx.event.EventHandler;
@@ -7,28 +8,39 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleResultsController extends Controller {
 	private final Controller lastController;
 	private final Assignment assignment;
+	private List<String> projectNames;
 	private final List<List<String>> answerList;
 	public GridPane grid;
+	public TextField locField;
+	public Text savedText;
 	public CheckBox filterCheck;
 	private ArrayList<ArrayList<VBox>> vboxes = new ArrayList<>();
 	private ArrayList<VBox> selected = new ArrayList<>();
+	private ArrayList<Double> grades = new ArrayList<>();
 
 	public MultipleResultsController(Controller lastController, Assignment assignment, List<String> projectNames, List<List<String>> answerList) {
 		this.lastController = lastController;
 		this.assignment = assignment;
+		this.projectNames = projectNames;
 		this.answerList = answerList;
 		for (int j = 0; j < projectNames.size(); j++) {
 			int correct = 0;
@@ -39,18 +51,41 @@ public class MultipleResultsController extends Controller {
 					correct++;
 			}
 			addLabel(projectNames.get(j), 0, j);
-			addLabel((int) (1000. * (correct * 10.) / (assignment.getQuestions().size() * .1)) / 1000. + "", 1, j);
+			double grade = (int) (1000. * (correct * 10.) / (assignment.getQuestions().size() * .1)) / 1000.;
+			addLabel(grade + "", 1, j);
+			grades.add(grade);
 		}
 		grid.getStyleClass().add("grid");
 		grid.setGridLinesVisible(true);
 	}
 
-	@FXML
-	private void mouseClick(MouseEvent e) {
-		Node source = (Node) e.getSource();
-		Integer colIndex = GridPane.getColumnIndex(source);
-		Integer rowIndex = GridPane.getRowIndex(source);
-//		System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
+	public void getFile() {
+		FileChooser filechooser = new FileChooser();
+		filechooser.setTitle("Open Auto-Grader Project");
+		Stage stage = new Stage();
+		File file = filechooser.showOpenDialog(stage);
+		if (file != null)
+			locField.setText(file.getName());
+	}
+
+	public void save() {
+		File file = new File(locField.getText());
+		if (file.isDirectory()) {
+			locField.setText(locField + "\\Output.txt");
+			save();
+			return;
+		}
+		file.delete();
+
+		try {
+			FileWriter writer = new FileWriter(file.getPath());
+			for (int i = 0; i < grades.size(); i++)
+				writer.write(projectNames.get(i).replace(":", "") + ": " + grades.get(i)+"\n");
+			writer.close();
+			savedText.setVisible(true);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public void addLabel(String s, int x, int y) {
